2e7c77cc0ba916fcda46bc3c11d2a90b85e61a1e
IsseTommy <1999yuto@gmail.com>
2021-02-08 13:44:51 +0900

    Change UI

--- app/src/main/java/com/example/count_ex/MainActivity.kt
+++ app/src/main/java/com/example/count_ex/MainActivity.kt
@@ -14,6 +14,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        changeUI()
+
         plusButton.setOnClickListener {
             num += 1
             changeUI()
